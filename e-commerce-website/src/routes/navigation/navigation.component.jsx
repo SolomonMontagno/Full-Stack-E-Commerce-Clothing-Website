6614792cd2136a053e9e4733e1504cee445789c0
Solomon Montagno <sjmontagno@gmail.com>
2023-10-16 11:42:31 -0400

    added firebase and google auth

--- e-commerce-website/src/routes/navigation/navigation.component.jsx
+++ e-commerce-website/src/routes/navigation/navigation.component.jsx
@@ -1,17 +1,21 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
-import './navigation.styles.scss'
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrwnLogo className='logo'/>
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-In">
+            SIGN IN
           </Link>
         </div>
       </div>

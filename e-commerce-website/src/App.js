fe2da875883dc4513722b2e75a4ef21e88f1cdc6
Solomon Montagno <sjmontagno@gmail.com>
2023-10-31 17:43:31 -0400

    Added checkout page with increment/decrement

--- e-commerce-website/src/App.js
+++ e-commerce-website/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route} from 'react-router-dom'
 import Navigation from './routes/navigation/navigation.component.jsx';
 import Authentication from './routes/authentication/authentication.component.jsx';
 import Shop from './routes/shop/shop.component.jsx';
-
+import CheckOut from './routes/checkout/checkout.componet.jsx';
 
 
 const App = () => {
@@ -14,6 +14,7 @@ const App = () => {
       <Route index element= {<Home/>} />
         <Route path='shop' element= {<Shop />} /> 
         <Route path='auth' element= {<Authentication />} />
+        <Route path='checkout' element= {<CheckOut/>} />
       </Route>
     </Routes>
     

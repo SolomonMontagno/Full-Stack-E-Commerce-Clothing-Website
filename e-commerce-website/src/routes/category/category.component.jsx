7978cfdb5120040db5a6bc8a0a4eaa0d7976a2db
Solomon Montagno <sjmontagno@gmail.com>
2023-11-21 16:16:08 -0500

    various imports

--- e-commerce-website/src/routes/category/category.component.jsx
+++ e-commerce-website/src/routes/category/category.component.jsx
@@ -1,8 +1,10 @@
 import './category.styles.scss'
 import { useParams } from 'react-router-dom'
+import { useContext } from 'react'
+import { CategoriesContext } from '../../contexts/categories.context'
 
 const Category = () => {
-    
+    { category } = useParams()
 }
 
 
